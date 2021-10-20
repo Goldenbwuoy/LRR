49d6f4997a6d7654993b927e7412d22756f83a14
Lan Hui <lanhui@zjnu.edu.cn>
2021-10-20 23:13:50 +0800

    Student.php: indent code

--- Student.php
+++ Student.php
@@ -1,20 +1,15 @@
 <?php
-$page="student";
+$page = "student";
 include 'Header.php';
 
 ?>
 
 <br><br><br>
 
-
 <div class="row" style="width:80%;margin:auto;">
-   
+
     <div class="col-md-6">
-     <h1> STUEDNT Account Created , Now you can Browse Course Portals </h1>   
+        <h1> STUEDNT Account Created , Now you can Browse Course Portals </h1>
     </div>
-    
-    
+
 </div>
-
-
-
